57f6db22d46d5b848ac0e5125165a7e241174af6
Oleg Vasilev <v.oleg2908@gmail.com>
2024-06-20 18:50:00 +0300

    delete unused imports

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,7 @@
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from '../../redux/store';
+import { store } from '../../redux/store';
 import { router } from '../../routes';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import { Loader } from '../ui/Loader/Loader';
 
 export const App = () => {
 	return (
